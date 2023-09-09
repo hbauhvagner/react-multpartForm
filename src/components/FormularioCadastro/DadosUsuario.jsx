562cba9bf5bd10213c7cff6f28a7c35c75a17e6c
hbauhvagner <hbauhvagner@gmail.com>
2023-09-09 20:56:28 -0300

    Entendendo o problema do cliente

---
+++ src/components/FormularioCadastro/DadosUsuario.jsx
@@ -0,0 +1,25 @@
+import { Button, TextField } from "@mui/material";
+
+export default function DadosUsuario() {
+    return (
+        <form>
+            <TextField 
+                id='email' 
+                label='email' 
+                type='email' 
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <TextField 
+                id='senha' 
+                label='senha' 
+                type='password' 
+                variant="outlined"
+                margin="normal"
+                fullWidth
+            />
+            <Button type='submit' variant="contained">Cadastrar</Button>
+        </form>
+    )
+}
