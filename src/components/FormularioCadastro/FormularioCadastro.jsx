7b1ca833b3832233ca6f54035401262675f62cec
hbauhvagner <hbauhvagner@gmail.com>
2023-09-10 01:27:41 -0300

    Renderização condicional

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,14 +1,32 @@
+import { useState } from "react";
 import DadosEntrega from "./DadosEntrega";
 import DadosPessoais from "./DadosPessoais";
 import DadosUsuario from "./DadosUsuario";
+import { Typography } from "@mui/material";
 
 export default function FormularioCadastro({ aoEnviar, validarCPF }) {
+    const [etapaAtual, setEtapaAtual] = useState(0)
+
+    function formularioAtual(etapa) {
+        switch (etapa) {
+            case 0:
+                return <DadosUsuario />
+            case 1:
+                return <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+            case 2:
+                return <DadosEntrega />
+            default:
+                return <Typography>Erro ao selecionar formulário</Typography>
+        }
+    }
 
     return (
         <>
-            <DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
-            <DadosUsuario />
-            <DadosEntrega />
+          {formularioAtual(etapaAtual)}    
         </>
     )
 }
+
+//<DadosPessoais aoEnviar={aoEnviar} validarCPF={validarCPF} />
+//<DadosUsuario />
+//<DadosEntrega />
