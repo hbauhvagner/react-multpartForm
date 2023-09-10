6d334b990fd2bdb190ff881b1cbf6f6fd0f2106a
hbauhvagner <hbauhvagner@gmail.com>
2023-09-10 15:29:05 -0300

    Trabalhando com estados

--- src/components/FormularioCadastro/DadosEntrega.jsx
+++ src/components/FormularioCadastro/DadosEntrega.jsx
@@ -1,9 +1,23 @@
 import { Button, TextField } from "@mui/material";
+import { useState } from "react";
 
-export default function DadosEntrega() {
+export default function DadosEntrega({aoEnviar}) {
+    const [cep, setCep] = useState('')
+    const [endereco, setEndereco] = useState('')
+    const [numero, setNumero] = useState('')
+    const [estado, setEstado] = useState('')
+    const [cidade, setCidade] = useState('')
+
     return (
-        <form>
+        <form onSubmit={(event) => {
+            event.preventDefault()
+            aoEnviar({cep, endereco, numero, estado, cidade})
+        }}>
             <TextField 
+                value={cep}
+                onChange={(event) => {
+                    setCep(event.target.value)
+                }}
                 id='cep' 
                 label='CEP' 
                 type='number' 
@@ -11,6 +25,10 @@ export default function DadosEntrega() {
                 margin="normal"
             />
             <TextField 
+                value={endereco}
+                onChange={(event) => {
+                    setEndereco(event.target.value)
+                }}
                 id='endereco' 
                 label='Endereço' 
                 type='text' 
@@ -19,6 +37,10 @@ export default function DadosEntrega() {
                 fullWidth
             />
             <TextField 
+                value={numero}
+                onChange={(event) => {
+                    setNumero(event.target.value)
+                }}
                 id='numero' 
                 label='Número' 
                 type='number' 
@@ -26,6 +48,10 @@ export default function DadosEntrega() {
                 margin="normal"
             />
             <TextField 
+                value={estado}
+                onChange={(event) => {
+                    setEstado(event.target.value)
+                }}
                 id='estado' 
                 label='Estado' 
                 type='text' 
@@ -33,6 +59,10 @@ export default function DadosEntrega() {
                 margin="normal"
             />
             <TextField 
+                value={cidade}
+                onChange={(event) => {
+                    setCidade(event.target.value)
+                }}
                 id='cidade' 
                 label='Cidade' 
                 type='text' 
